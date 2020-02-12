95310fd805461ed76f49e22042b0ed34a21ed170
david <al221511662@gmail.com>
2020-02-11 22:06:11 -0600

    version 1

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let btnMenu= document.querySelector('.btn-menu')
+let menu= document.querySelector('.list-container')
+let containerMenu= document.querySelector('.menu')
+let activador=true;
+
+btnMenu.addEventListener('click', ()=>{
+
+	document.querySelector('btn-menu')
+
+	if(activador){
+
+	menu.style.left="0";
+	menu.style.transition="0.5s";
+	activador=false;
+
+	}else{
+
+		activador=false;
+
+		menu.style.left="-100%";
+		menu.style.transition ="0.5s";
+		activador=true;
+
+	}
+
+});
+
+let enlaces = document.querySelectorAll('.lists li a')
+
+enlaces.forEach((element)=>{
+
+	element.addEventListener('click',(event)=>{
+
+		enlaces.forEach((link)=>{
+
+			link.classList.remove('activo')
+		})
+
+
+		event.target.classList.add('activo')
+		})
+})
